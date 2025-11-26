08240ce8fe549789b48f82dac3be23d9b10bb069
Charles Randicha <charles.randicha@gmail.com>
2025-11-26 22:06:24 +0700

    feat: prepare basic logic for input and validation

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  imports: [FormsModule],
 })
 export class HomeComponent {
-  protected readonly title = signal('Home');
-}
+  protected readonly inputValue = signal('');
+  protected readonly inputValueValidation = computed(() => {
+    if (this.inputValue() === '')
+      return {
+        success: false,
+        message: '',
+      };
 
+    return {
+      success: true,
+      message: 'Valid ACN Number',
+    };
+  });
+}
